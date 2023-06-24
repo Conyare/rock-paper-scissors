9ca1edd453a5fc7667fa4fe8e3c1830bfeefc303
Connor Handy <connorhandy34211@gmail.com>
2023-06-23 20:26:33 -0400

    Create playRound function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 function getComputerChoice() {
+
+    /*Randomly generate a choice of 
+    rock, paper, or scissors for the computer */
     
     choice = Math.floor(Math.random() * 3) + 1;
 
@@ -14,3 +17,67 @@ function getComputerChoice() {
             break;
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    
+    playerSelectionLower = playerSelection.toLowerCase();
+
+    computerSelectionLower = computerSelection.toLowerCase();
+
+    winMessage = "You Win! " + playerSelection + " beats " 
+    + computerSelection;
+
+    loseMessage = "You Lose! " + computerSelection + " beats " 
+    + playerSelection; 
+
+    tieMessage = "Tie game! Go again!"
+
+     if (playerSelectionLower === "rock") {
+        
+        if (computerSelectionLower === "scissors") {
+            return winMessage;
+        }
+
+        else if (computerSelectionLower == "paper") {
+            return loseMessage;
+        }
+
+        else {
+            return tieMessage;
+        }
+     }
+
+     if (playerSelection === "paper") {
+
+        if (computerSelection === "rock") {
+            return winMessage;
+        }
+
+        else if (computerSelection === "scissors") {
+            return loseMessage;
+        }
+
+        else {
+            return tieMessage;
+        }
+     }
+    
+     if (playerSelection === "scissors") {
+
+        if (computerSelection === "paper") {
+            return winMessage;
+        }
+
+        else if (computerSelection === "rock") {
+            return loseMessage;
+        }
+
+        else {
+            return tieMessage;
+        }
+     }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
